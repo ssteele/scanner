0d58986e116cf420486ee1b76d9916901b567f95
Steve Steele <steven.harville.steele@gmail.com>
2024-01-20 13:58:03 -0600

    Add grocery constants

---
+++ constants/groceries.js
@@ -0,0 +1,72 @@
+export const GROCERIES = [
+    {
+        id: 'egg',
+        name: 'Egg',
+        price: 0.52,
+    },
+    {
+        id: 'banana',
+        name: 'Banana',
+        price: 0.47,
+    },
+    {
+        id: 'bellPepper',
+        name: 'Bell pepper',
+        price: 1.55,
+    },
+    {
+        id: 'swissCheese',
+        name: 'Swiss cheese',
+        price: 3.10,
+    },
+    {
+        id: 'blackBerries',
+        name: 'Black berries',
+        price: 4.95,
+    },
+    {
+        id: 'flower',
+        name: 'Flower',
+        price: 8.30,
+    },
+    {
+        id: 'cucumber',
+        name: 'Cucumber',
+        price: 2.56,
+    },
+    {
+        id: 'lime',
+        name: 'Lime',
+        price: 0.39,
+    },
+    {
+        id: 'grapes',
+        name: 'Grapes',
+        price: 7.24,
+    },
+    {
+        id: 'carrot',
+        name: 'Carrot',
+        price: 0.39,
+    },
+    {
+        id: 'raspberries',
+        name: 'Raspberries',
+        price: 5.16,
+    },
+    {
+        id: 'apple',
+        name: 'Apple',
+        price: 1.12,
+    },
+    {
+        id: 'cornOnTheCob',
+        name: 'Corn on the cob',
+        price: 0.57,
+    },
+    {
+        id: 'cake',
+        name: 'Cake',
+        price: 3.93,
+    },
+];
