b0bcc311f7c148ba4e605d8851f628eb76684fa6
Steve Steele <steven.harville.steele@gmail.com>
2024-01-21 15:39:58 -0600

    Detect objects

--- src/script.js
+++ src/script.js
@@ -1,25 +1,53 @@
 const video = document.getElementById('video');
 
-async function getMediaPermission(constraints) {
-  let stream = null;
+const detectFrame = (video, model) => {
+  model.detect(video)
+    .then(predictions => {
+      console.log('SHS predictions:', predictions); // @debug
+      this.renderPredictions(predictions);
+      // requestAnimationFrame(() => {
+      //   detectFrame(video, model);
+      // });
+    });
+};
+
+const getMedia = async (constraints) => {
+  // let stream = null;
 
   try {
-    stream = await navigator.mediaDevices.getUserMedia(constraints);
-    console.log('SHS stream:', stream); // @debug
-    video.srcObject = stream;
-    video.play();
-  } catch (err) {
-    console.log('SHS err:', err); // @debug
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(stream => {
+          video.srcObject = stream;
+          return new Promise((resolve) => {
+            video.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+
+      const modelPromise = cocoSsd.load();
+
+      Promise.all([modelPromise, webCamPromise])
+        .then((values) => {
+          detectFrame(video, values[0]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      }
+  } catch (error) {
+    console.error(error);
   }
 }
 
 navigator.permissions.query({ name: 'camera' })
-  .then(({ state }) => {
-    console.log('SHS state:', state); // @debug
-    // if (['denied', 'prompt'].includes(state)) {
-      getMediaPermission({video: true});
-    // }
+  .then(() => {
+    getMedia({
+      video: { facingMode: 'environment' },
+    });
   })
   .catch((error) => {
     console.log('SHS error:', error); // @debug
-  })
+  });
