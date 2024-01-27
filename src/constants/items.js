7e9f100915aced23bf544d180b28709b231f4588
Steve Steele <steven.harville.steele@gmail.com>
2024-01-27 15:55:09 -0600

    Add items

--- src/constants/items.js
+++ src/constants/items.js
@@ -69,4 +69,259 @@ export const ITEMS = [
         name: 'Cake',
         price: 3.93,
     },
+    {
+        id: 'cellPhone',
+        name: 'Cell phone',
+        price: 790.00,
+    },
+    {
+        id: 'tv',
+        name: 'TV',
+        price: 250.00,
+    },
+    {
+        id: 'oven',
+        name: 'Oven',
+        price: 950.00,
+    },
+    {
+        id: 'cup',
+        name: 'Cup',
+        price: 2.50,
+    },
+    {
+        id: 'vase',
+        name: 'Vase',
+        price: 9.99,
+    },
+    {
+        id: 'bottle',
+        name: 'Bottle',
+        price: 9.99,
+    },
+    {
+        id: 'diningTable',
+        name: 'Dining table',
+        price: 1200.00,
+    },
+    {
+        id: 'sink',
+        name: 'Sink',
+        price: 500.00,
+    },
+    {
+        id: 'couch',
+        name: 'Couch',
+        price: 2250.00,
+    },
+    {
+        id: 'spoon',
+        name: 'Spoon',
+        price: 0.99,
+    },
+    {
+        id: 'mouse',
+        name: 'Mouse',
+        price: 15.00,
+    },
+    {
+        id: 'knife',
+        name: 'Knife',
+        price: 10.99,
+    },
+    {
+        id: 'laptop',
+        name: 'Laptop',
+        price: 1000.00,
+    },
+    {
+        id: 'microwave',
+        name: 'Microwave',
+        price: 400.00,
+    },
+    {
+        id: 'bench',
+        name: 'Bench',
+        price: 60.00,
+    },
+    {
+        id: 'refrigerator',
+        name: 'Refrigerator',
+        price: 1450.00,
+    },
+    {
+        id: 'chair',
+        name: 'Chair',
+        price: 40.00,
+    },
+    {
+        id: 'book',
+        name: 'Book',
+        price: 9.00,
+    },
+    {
+        id: 'cat',
+        name: 'Cat',
+        price: null,
+    },
+    {
+        id: 'toothbrush',
+        name: 'Toothbrush',
+        price: 10.00,
+    },
+    {
+        id: 'scissors',
+        name: 'Scissors',
+        price: 17.00,
+    },
+    {
+        id: 'remote',
+        name: 'Remote',
+        price: 25.00,
+    },
+    {
+        id: 'bed',
+        name: 'Bed',
+        price: 750.00,
+    },
+    {
+        id: 'teddyBear',
+        name: 'Teddy bear',
+        price: 20.00,
+    },
+    {
+        id: 'elephant',
+        name: 'Elephant',
+        price: null,
+    },
+    {
+        id: 'handbag',
+        name: 'Handbag',
+        price: 35.00,
+    },
+    {
+        id: 'keyboard',
+        name: 'Keyboard',
+        price: 45.00,
+    },
+    {
+        id: 'donut',
+        name: 'Donut',
+        price: 1.89,
+    },
+    {
+        id: 'orange',
+        name: 'Orange',
+        price: 1.59,
+    },
+    {
+        id: 'sandwich',
+        name: 'Sandwich',
+        price: 9.00,
+    },
+    {
+        id: 'broccoli',
+        name: 'Broccoli',
+        price: 1.97,
+    },
+    {
+        id: 'pizza',
+        name: 'Pizza',
+        price: 22.00,
+    },
+    {
+        id: 'hotDog',
+        name: 'Hot dog',
+        price: 5.00,
+    },
+    {
+        id: 'sportsBall',
+        name: 'Ball',
+        price: 17.00,
+    },
+    {
+        id: 'tie',
+        name: 'Tie',
+        price: 20.00,
+    },
+    {
+        id: 'toilet',
+        name: 'Toilet',
+        price: 435.00,
+    },
+    {
+        id: 'frisbee',
+        name: 'Frisbee',
+        price: 17.00,
+    },
+    {
+        id: 'sheep',
+        name: 'Sheep',
+        price: null,
+    },
+    {
+        id: 'tennisRacket',
+        name: 'Tennis racket',
+        price: 35.00,
+    },
+    {
+        id: 'stopSign',
+        name: 'Stop sign',
+        price: 87.50,
+    },
+    {
+        id: 'kite',
+        name: 'Kite',
+        price: 23.00,
+    },
+    {
+        id: 'parkingMeter',
+        name: 'Parking meter',
+        price: 3.00,
+    },
+    {
+        id: 'suitcase',
+        name: 'Suitcase',
+        price: 50.00,
+    },
+    {
+        id: 'trafficLight',
+        name: 'Traffic light',
+        price: 500000.00,
+    },
+    {
+        id: 'umbrella',
+        name: 'Umbrella',
+        price: 20.00,
+    },
+    {
+        id: 'bus',
+        name: 'Bus',
+        price: 265000.00,
+    },
+    {
+        id: 'motorcycle',
+        name: 'Motorcycle',
+        price: 7500.00,
+    },
+    {
+        id: 'baseballGlove',
+        name: 'Baseball glove',
+        price: 20.00,
+    },
+    {
+        id: 'surfboard',
+        name: 'Surfboard',
+        price: 750.00,
+    },
+    {
+        id: 'pottedPlant',
+        name: 'Potted plant',
+        price: 63.00,
+    },
+    {
+        id: 'airplane',
+        name: 'Airplane',
+        price: 250000000.00,
+    },
 ];
